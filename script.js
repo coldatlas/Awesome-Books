007b91cbbcc93d0b7b7a0c840fcc9c88303cf431
Mina <cccoldatlas@gmail.com>
2021-07-05 20:01:22 +0200

    Fix html and js linter errors

--- script.js
+++ script.js
@@ -1,44 +1,45 @@
 const savedData = localStorage.getItem('savedData');
 let book = [];
-const bookList = document.getElementById("bookList") ;
+const bookList = document.getElementById('bookList');
 
 function showItems() {
   bookList.innerHTML = '';
   book.forEach(book => {
-    add(book)
-  })
+    add(book);
+  });
 }
 
-function add({title, author, id}){
-    const li = document.createElement("li");
-    const button = document.createElement("button");
-    button.innerText = "Remove";
-    button.addEventListener('click',() => {
-      book = book.filter(item => item.id !== id)
-      showItems()
-      localStorage.setItem('savedData', JSON.stringify(book))
-    });   
-   
-    
-  li.appendChild(document.createTextNode(title + ' '));
+function add({ title, author, id }) {
+  const li = document.createElement('li');
+  const button = document.createElement('button');
+  button.innerText = 'Remove';
+  button.addEventListener('click', () => {
+    book = book.filter(item => item.id !== id);
+    showItems();
+    localStorage.setItem('savedData', JSON.stringify(book));
+  });
+
+  li.appendChild(document.createTextNode(title));
   li.appendChild(document.createElement('br'));
-  li.appendChild(document.createTextNode(author + ' '));
+  li.appendChild(document.createTextNode(author));
   li.appendChild(button);
   li.appendChild(document.createElement('hr'));
-  bookList.appendChild(li); 
+  bookList.appendChild(li);
 }
 
-if(savedData) {
-  book = JSON.parse(savedData)
+if (savedData) {
+  book = JSON.parse(savedData);
   showItems();
 }
 
 const form = document.getElementById('bookAndAuthor');
 form.addEventListener('submit', (event) => {
-  event.preventDefault()
-  const bookInput = document.getElementById("bookInput");
-  const authorInput = document.getElementById("authorInput");
-  book.push({title:bookInput.value, author:authorInput.value, id: Math.floor(Math.random() * 1000)});
-  showItems()
-  localStorage.setItem('savedData', JSON.stringify(book))
-})
+  event.preventDefault();
+  const bookInput = document.getElementById('bookInput');
+  const authorInput = document.getElementById('authorInput');
+  book.push( {
+      title: bookInput.value, author: authorInput.value, id: Math.floor(Math.random() * 1000)
+    });
+  showItems();
+  localStorage.setItem('savedData', JSON.stringify(book));
+});
