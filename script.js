636cc3b9fa0629c532cd956b334702d526a773f9
Mina <cccoldatlas@gmail.com>
2021-07-08 11:55:47 +0200

    style page to match wireframe

--- script.js
+++ script.js
@@ -22,15 +22,16 @@ class BookLibrary {
       bookList.removeChild(li);
       localStorage.setItem('savedData', JSON.stringify(library.book));
     });
-  
+    li.appendChild(document.createTextNode('"'));
     li.appendChild(document.createTextNode(title));
+    li.appendChild(document.createTextNode('"'));
     li.appendChild(document.createTextNode(' '));
     li.appendChild(document.createTextNode('by'));
     li.appendChild(document.createTextNode(' '));
     li.appendChild(document.createTextNode(author));
     li.appendChild(button);
-    li.appendChild(document.createElement('hr'));
     bookList.appendChild(li);
+    
   }
 }
 
