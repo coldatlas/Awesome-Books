a2f8fc314cba0e1518bc9438196af315e22afca9
Mina <cccoldatlas@gmail.com>
2021-07-09 11:40:46 +0200

    adjust js and css

--- script.js
+++ script.js
@@ -64,24 +64,24 @@ window.addEventListener('load', () => {
   library.showItems();
 });
 
-var one = document.getElementById("bookList");
-var two = document.getElementById("add_new");
-var three = document.getElementById("contact");
+let one = document.getElementById("bookList");
+let two = document.getElementById("add_new");
+let three = document.getElementById("contact");
 
 function listFun() {
-  one.style.display = "block";
-  two.style.display = "none";
-  three.style.display = "none";
+  one.style.display = 'block';
+  two.style.display = 'none';
+  three.style.display = 'none';
 }
 
 function addFun() {
-  one.style.display = "none";
-  two.style.display = "block";
-  three.style.display = "none";
+  one.style.display = 'none';
+  two.style.display = 'block';
+  three.style.display = 'none';
 }
 
 function contactFun() {
-  one.style.display = "none";
-  two.style.display = "none";
-  three.style.display = "block";
+  one.style.display = 'none';
+  two.style.display = 'none';
+  three.style.display = 'block';
 }
